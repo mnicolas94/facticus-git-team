19ce0a9b769e72428c7491342fd276a023765716
mnicolas94 <miguelcok27@gmail.com>
2022-08-30 16:12:23 -0400

    test samples

---
+++ Samples/TestSo.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace GitTeam.Samples
+{
+    [CreateAssetMenu(fileName = "TestSo", menuName = "TestSo", order = 0)]
+    public class TestSo : ScriptableObject
+    {
+        [SerializeField] private string _test;
+    }
+}
